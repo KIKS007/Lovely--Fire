39bae54f7375087d29e44a24160fab41c69a8d9a
Keryann Malacchina <keryannm@gmail.com>
2017-02-01 12:00:35 +0100

    Project Re-Imported

---
+++ Assets/Scripts/JoinRandom.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using Photon;
+
+/// <summary>
+/// This script automatically connects to Photon (using the settings file),
+/// tries to join a random room and creates one if none was found (which is ok).
+/// </summary>
+public class JoinRandom : PunBehaviour
+{
+	[Header ("Lobby")]
+	public string lobbyName = "Lobby";
+	public string levelToLoadPath = "";
+
+
+	public virtual void Start()
+	{
+		PhotonNetwork.automaticallySyncScene = true;
+		PhotonNetwork.CrcCheckEnabled = true;
+		PhotonNetwork.ConnectUsingSettings("v1.0");
+	}
+
+	public override void OnJoinedLobby ()
+	{
+		RoomOptions roomOptions = new RoomOptions ();
+		roomOptions.MaxPlayers = 0;
+
+		PhotonNetwork.JoinOrCreateRoom ("room1", roomOptions, null);
+	}
+
+	void OnGUI() {
+		GUI.Label (new Rect(10, 10, 100, 30), "players: " + PhotonNetwork.playerList.Length);
+
+		if (PhotonNetwork.isMasterClient && GUI.Button (new Rect (10, 40, 100, 30), "start")) 
+		{
+			PhotonNetwork.LoadLevel (levelToLoadPath);
+		}
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TrueSync;
+
+public class Player : TrueSyncBehaviour 
+{
+	[Header ("Movement")]
+	public FP movementSpeed;
+
+	protected const byte INPUT_MOVE = 0;
+	protected const byte INPUT_AIM = 1;
+
+	protected const byte INPUT_FIRE = 2;
+
+	private TSRigidBody rigidBody;
+
+	public override void OnSyncedStart ()
+	{
+		rigidBody = GetComponent<TSRigidBody> ();
+	}
+
+	public override void OnSyncedInput ()
+	{
+		TSVector movement = new TSVector ((FP) Input.GetAxis ("Horizontal"), 0, (FP) Input.GetAxis ("Vertical"));
+
+		TrueSyncInput.SetTSVector (INPUT_MOVE, movement);
+	}
+
+	public override void OnSyncedUpdate ()
+	{
+		Debug.Log (TrueSyncInput.GetTSVector (INPUT_MOVE));
+
+		TSVector movement = TrueSyncInput.GetTSVector (INPUT_MOVE);
+		
+		rigidBody.AddForce (movement * movementSpeed - rigidBody.velocity, ForceMode.Impulse);
+
+		Movement ();
+	}
+
+	void Movement ()
+	{
+	}
+
+	public override void OnPlayerDisconnection (int playerId)
+	{
+		
+	}
+}
