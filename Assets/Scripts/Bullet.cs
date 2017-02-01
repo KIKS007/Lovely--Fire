081c2233e11dfec9246f4152c14ccea26e352253
Keryann Malacchina <keryannm@gmail.com>
2017-02-01 15:43:01 +0100

    Fire Done

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TrueSync;
+
+public class Bullet : TrueSyncBehaviour 
+{
+	[Header ("Bullet")]
+	public FP bulletSpeed = 2;
+
+	private TSRigidBody rigidBody;
+
+	public override void OnSyncedStart ()
+	{
+		rigidBody = GetComponent<TSRigidBody> ();
+
+		rigidBody.AddForce (tsTransform.forward * bulletSpeed, ForceMode.Impulse);
+	}
+
+	public override void OnSyncedUpdate ()
+	{
+		TSVector velocity = rigidBody.velocity;
+		velocity.y = 0;
+
+		//rigidBody.AddForce (tsTransform.forward * bulletSpeed - velocity, ForceMode.Impulse);
+	}
+}
